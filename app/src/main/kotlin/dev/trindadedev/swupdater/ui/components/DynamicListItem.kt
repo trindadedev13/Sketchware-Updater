abeff50e02db7b9280e3eb3159fb15d33c118cb2
trindadedev13 <devsuay@gmail.com>
2024-11-11 13:39:28 -0300

    fix: Modifier not found

--- app/src/main/kotlin/dev/trindadedev/swupdater/ui/components/DynamicListItem.kt
+++ app/src/main/kotlin/dev/trindadedev/swupdater/ui/components/DynamicListItem.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.unit.*
+import androidx.compose.ui.Modifier
 
 @Composable
 fun DynamicListItem(
