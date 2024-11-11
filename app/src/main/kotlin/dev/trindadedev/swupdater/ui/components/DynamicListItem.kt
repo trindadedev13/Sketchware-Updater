ede973285c16e2321aed42cb837367542dfd9a68
trindadedev13 <devsuay@gmail.com>
2024-11-11 13:32:29 -0300

    fix: Content padding not exists

--- app/src/main/kotlin/dev/trindadedev/swupdater/ui/components/DynamicListItem.kt
+++ app/src/main/kotlin/dev/trindadedev/swupdater/ui/components/DynamicListItem.kt
@@ -7,21 +7,22 @@ import androidx.compose.ui.unit.*
 
 @Composable
 fun DynamicListItem(
-    listLength: Int,
-    currentValue: Int,
-    content: @Composable () -> Unit,
+  modifier: Modifier = Modifier,
+  listLength: Int,
+  currentValue: Int,
+  content: @Composable () -> Unit,
 ) {
-    val shape = when (currentValue) {
-        0 -> ItemShape.Top
-        listLength -> ItemShape.Bottom
-        else -> ItemShape.Middle
-    }
-    
-    Card(
-        shape = shape,
-        elevation = CardDefaults.cardElevation(0.dp),
-        contentPadding = PaddingValues(0.dp)
-    ) {
-        content()
-    }
+  val shape = when (currentValue) {
+    0 -> ItemShape.Top
+    listLength -> ItemShape.Bottom
+    else -> ItemShape.Middle
+  }
+  
+  Card(
+    modifier = modifier,
+    shape = shape,
+    elevation = CardDefaults.cardElevation(0.dp),
+  ) {
+    content()
+  }
 }
