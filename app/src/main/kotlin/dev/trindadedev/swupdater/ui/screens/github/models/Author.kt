3a0057c5597571bc7e8ccf3f44fff7ee972984bc
trindadedev13 <devsuay@gmail.com>
2024-11-11 13:46:23 -0300

    fix: ',' expected

--- app/src/main/kotlin/dev/trindadedev/swupdater/ui/screens/github/models/Author.kt
+++ app/src/main/kotlin/dev/trindadedev/swupdater/ui/screens/github/models/Author.kt
@@ -2,6 +2,6 @@ package dev.trindadedev.swupdater.ui.screens.github.models
 
 data class Author(
   val name: String,
-  val date: String
+  val date: String,
   val avatar: String = "https://github.com/$name.png"
 )
