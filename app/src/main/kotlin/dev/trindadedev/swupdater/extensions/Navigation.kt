73723c91a63fd7f7d7037a2cffd740ddc053bd3e
trindadedev13 <devsuay@gmail.com>
2024-11-10 21:44:01 -0300

    fix: route not is valid type

--- app/src/main/kotlin/dev/trindadedev/swupdater/extensions/Navigation.kt
+++ app/src/main/kotlin/dev/trindadedev/swupdater/extensions/Navigation.kt
@@ -2,7 +2,7 @@ package dev.trindadedev.swupdater.extensions
 
 import androidx.navigation.NavHostController
 
-fun <T> NavHostController.navigateSingleTop(T: route) {
+fun <T> NavHostController.navigateSingleTop(route: T) {
   this.navigate(route) {
     popUpTo(this.graph.startDestinationId)
     launchSingleTop = true
