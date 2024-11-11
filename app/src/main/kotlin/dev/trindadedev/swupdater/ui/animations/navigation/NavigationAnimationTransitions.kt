cc9666f894106f2ab0f045d293caf25e5b7b8a1f
trindadedev13 <devsuay@gmail.com>
2024-11-11 02:39:38 -0300

    feat: use Horizontaly

--- app/src/main/kotlin/dev/trindadedev/swupdater/ui/animations/navigation/NavigationAnimationTransitions.kt
+++ app/src/main/kotlin/dev/trindadedev/swupdater/ui/animations/navigation/NavigationAnimationTransitions.kt
@@ -3,20 +3,20 @@ package dev.trindadedev.swupdater.ui.animations.navigation
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 
 object NavigationAnimationTransitions {
 
   val enterTransition =
-    fadeIn(tween(250)) + slideInVertically { it / 2 }
+    fadeIn(tween(250)) + slideInHorizontally { it / 2 }
 
   val exitTransition =
-    fadeOut(tween(200)) + slideOutVertically { -it / 2 }
+    fadeOut(tween(200)) + slideOutHorizontally { -it / 2 }
 
   val popEnterTransition =
-    fadeIn(tween(250)) + slideInVertically { -it / 2 }
+    fadeIn(tween(250)) + slideInHorizontally { -it / 2 }
 
   val popExitTransition =
-    fadeOut(tween(200)) + slideOutVertically { it / 2 }
+    fadeOut(tween(200)) + slideOutHorizontally { it / 2 }
 }
