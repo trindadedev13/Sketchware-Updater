75d6ff3093aa76a678c43c1986a63cb98d2725f9
trindadedev13 <devsuay@gmail.com>
2024-11-11 14:16:39 -0300

    fix: vars type

--- app/src/main/kotlin/dev/trindadedev/swupdater/ui/screens/github/models/Author.kt
+++ app/src/main/kotlin/dev/trindadedev/swupdater/ui/screens/github/models/Author.kt
@@ -3,7 +3,7 @@ package dev.trindadedev.swupdater.ui.screens.github.models
 import kotlinx.datetime.Instant
 
 data class Author(
-  val name: Instant,
-  val date: String,
+  val name: String,
+  val date: Instant,
   val avatar: String = "https://github.com/$name.png"
 )
