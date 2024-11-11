dcb1875d08b3bf8dc76601165dd8123222bfd949
trindadedev13 <devsuay@gmail.com>
2024-11-11 13:59:16 -0300

    fix: Instant expected

--- app/src/main/kotlin/dev/trindadedev/swupdater/ui/screens/github/models/Author.kt
+++ app/src/main/kotlin/dev/trindadedev/swupdater/ui/screens/github/models/Author.kt
@@ -1,7 +1,9 @@
 package dev.trindadedev.swupdater.ui.screens.github.models
 
+import kotlinx.datetime.Instant
+
 data class Author(
-  val name: String,
+  val name: Instant,
   val date: String,
   val avatar: String = "https://github.com/$name.png"
 )
