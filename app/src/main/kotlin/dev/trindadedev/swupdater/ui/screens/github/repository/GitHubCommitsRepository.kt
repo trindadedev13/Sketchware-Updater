f7164cca822b55cde6165c1aa9a6d9c3da855b14
trindadedev13 <devsuay@gmail.com>
2024-11-11 14:00:11 -0300

    chore: cleanup unused imports

--- app/src/main/kotlin/dev/trindadedev/swupdater/ui/screens/github/repository/GitHubCommitsRepository.kt
+++ app/src/main/kotlin/dev/trindadedev/swupdater/ui/screens/github/repository/GitHubCommitsRepository.kt
@@ -1,12 +1,5 @@
 package dev.trindadedev.swupdater.ui.screens.github.repository
 
-import io.ktor.client.*
-import io.ktor.client.call.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.serialization.gson.*
-
 import kotlinx.coroutines.runBlocking
 
 import dev.trindadedev.swupdater.ui.screens.github.models.Commit
